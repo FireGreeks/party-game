b868f9b7adaf549b48d55fbc49fc137c79c2571b
FireGreeks <theodorbischler1@gmail.com>
2020-06-30 21:28:01 +0200

    Set for server

--- Party Game/Assets/Scripts/ServerInfo.cs
+++ Party Game/Assets/Scripts/ServerInfo.cs
@@ -5,7 +5,8 @@ using UnityEngine.Networking;
 
 public static class ServerInfo
 {
-    public static string ServerURL = "http://127.0.0.1:8000/SERVER";
+    //public static string ServerURL = "http://127.0.0.1:8000/SERVER";
+    public static string ServerURL = "https://party-game-mobile.herokuapp.com/SERVER";
     public static string RoomURL;
     public static bool hasRoom = false;
 }
